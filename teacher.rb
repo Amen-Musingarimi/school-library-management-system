7887e7ed8cd87dd63f0dd8a07555df62627b64f4
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-05-15 12:17:01 +0200

    Add person, student and teacher classess

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require_relative 'person'
+
+class Teacher < Person
+  def initialize(name = "Unknown", age = 0, parent_permission = true, specialization)
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+
